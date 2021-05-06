fa76113adfe5cf2e8d05ccefbc36ca596b7dbb78
David <david.helman@mail.huji.ac.il>
2021-05-06 11:44:20 +0300

    Update 7_Display_both_images_together.py

--- codes_lesson_5/7_Display_both_images_together.py
+++ codes_lesson_5/7_Display_both_images_together.py
@@ -6,6 +6,20 @@ plt.rcParams.update({'font.size': 12})
 # set panel for plots
 fig, (ax1, ax2)= plt.subplots(ncols=2, nrows=1)
 
+# Read WRF tile, replace no values with nan and convert to degrees C
+array1                  = raster_wrf.read(1) 
+array1[array1 == 32768] = 'nan'
+array1                  = array1 - 273.15
+img1                    = ax1.imshow(array1,cmap='jet')
+
+# Read MODIS clipped tile, replace no values with nan and convert to degrees C
+# (notice that there's a factor required to get true values for MOD11A1 product)
+array2                  = clipped_MOD.read(1)
+array2                  = array2 * 1.0
+array2[array2 == 0]     = np.nan
+array2                  = array2 * 0.02 - 273.15
+img2                    = ax2.imshow(array2,cmap='jet')
+
 # set colorbar, axes, and title for WRF
 cbar1 = fig.colorbar(img1, ax=ax1)
 cbar1.ax.set_ylabel('Temp [\N{DEGREE SIGN}C]')
@@ -23,19 +37,5 @@ ax2.set_xlabel('x')
 ax2.set_ylabel('y')
 ax2.set_title("MODIS")
 ax2.set_aspect('auto')
-
-# Read WRF tile, replace no values with nan and convert to degrees C
-array1                  = raster_wrf.read(1) 
-array1[array1 == 32768] = 'nan'
-array1                  = array1 - 273.15
-img1                    = ax1.imshow(array1,cmap='jet')
-
-# Read MODIS clipped tile, replace no values with nan and convert to degrees C
-# (notice that there's a factor required to get true values for MOD11A1 product)
-array2                  = clipped_MOD.read(1)
-array2                  = array2 * 1.0
-array2[array2 == 0]     = np.nan
-array2                  = array2 * 0.02 - 273.15
-img2                    = ax2.imshow(array2,cmap='jet')
 
 plt.tight_layout(h_pad=1)
