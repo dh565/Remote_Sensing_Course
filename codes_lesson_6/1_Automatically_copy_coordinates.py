a3652bea687cbba98886e62e3163ac109b364532
David <david.helman@mail.huji.ac.il>
2021-05-12 19:28:17 +0300

    Create 1_Automatically_copy_coordinates.py

---
+++ codes_lesson_6/1_Automatically_copy_coordinates.py
@@ -0,0 +1,4 @@
+minx, miny = raster_wrf.bounds[0], raster_wrf.bounds[1] # copy the coordinates to HERE...
+maxx, maxy = raster_wrf.bounds[2], raster_wrf.bounds[3]
+bbox = box(minx, miny, maxx, maxy)
+print(bbox)
