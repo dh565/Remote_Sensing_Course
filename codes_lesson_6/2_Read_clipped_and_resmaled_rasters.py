58dbccf6119e97b164444a50672c0bf5b6104799
David <david.helman@mail.huji.ac.il>
2021-05-13 17:02:50 +0300

    Update 2_Read_clipped_and_resmaled_rasters.py

--- codes_lesson_6/2_Read_clipped_and_resmaled_rasters.py
+++ codes_lesson_6/2_Read_clipped_and_resmaled_rasters.py
@@ -1,5 +1,6 @@
 # Use rasterio to read your clipped and resampled rasters
-resm_WRF1030 = rasterio.open('/content/WRF1030_resampled_to_1km.tif')
-resm_WRF1330 = rasterio.open('/content/WRF1330_resampled_to_1km.tif')
-MOD1030_clip = rasterio.open('/content/Clipped_new_MOD11A1_03022019_1030.tif')
-MYD1330_clip = rasterio.open('/content/Clipped_new_MYD11A1_03022019_1330.tif')
+# Use rasterio to read your clipped and resampled rasters
+resm_WRF1030 = rasterio.open('/content/drive/MyDrive/gdrive_folder/WRF1030_resampled_to_1km.tif')
+resm_WRF1330 = rasterio.open('/content/drive/MyDrive/gdrive_folder/WRF1330_resampled_to_1km.tif')
+MOD1030_clip = rasterio.open('/content/drive/MyDrive/gdrive_folder/Clipped_new_MOD11A1_03022019_1030.tif')
+MYD1330_clip = rasterio.open('/content/drive/MyDrive/gdrive_folder/Clipped_new_MYD11A1_03022019_1330.tif')
