9e698b1967ff9f93422d3e229631768386f721ab
David <david.helman@mail.huji.ac.il>
2021-05-20 12:02:04 +0300

    Create 8_Display_with_rasterio.py

---
+++ codes_lesson_7/8_Display_with_rasterio.py
@@ -0,0 +1,33 @@
+from matplotlib import ticker
+import cartopy.crs as ccrs
+
+file_new    = '/content/drive/MyDrive/gdrive_folder/Clipped_Resampled_1km_LSASAF_MSG_DSSF.tif'
+LSASAF_DSSF = xr.open_rasterio(file_new)
+
+print(LSASAF_DSSF.nodatavals)
+
+array_input = LSASAF_DSSF
+
+plt.figure(figsize=(9,6))
+
+ax1 = plt.axes(projection=ccrs.Robinson())
+ax1.coastlines()
+ax1.add_feature(cartopy.feature.OCEAN)
+ax1.add_feature(cartopy.feature.LAKES, alpha=1.0)
+ax1.add_feature(cartopy.feature.RIVERS)
+
+#gl = ax.gridlines(draw_labels=False)
+p1 = array_input.plot(ax=ax1,
+             levels=15, vmin=50, vmax=80,
+             cmap='viridis',
+             transform=ccrs.PlateCarree(),
+             add_colorbar=False)
+
+plt.title('DSSF at 10:00')
+
+ax_cb = plt.axes([0.325, 0.08, 0.40, 0.02])
+tick_locator = ticker.MaxNLocator(nbins=8)
+cb = plt.colorbar(p1, cax=ax_cb, orientation='horizontal')
+cb.locator = tick_locator
+cb.update_ticks()
+cb.ax.set_xlabel('Downward Shortwave Surface flux (W m$^{-2}$)');
