64e130ca7440a05a7efce51da2c9d99383a81433
David <david.helman@mail.huji.ac.il>
2021-06-02 21:40:29 +0300

    Create Connect_to_GEE.py

---
+++ Connect_to_GEE.py
@@ -0,0 +1,10 @@
+# Import GEE for python and get into your account
+import ee
+
+# Trigger the authentication flow.
+ee.Authenticate()
+
+# Initialize the library.
+ee.Initialize()
+
+# Remember, you need to be conected to your primary HUJI gamil account first!
