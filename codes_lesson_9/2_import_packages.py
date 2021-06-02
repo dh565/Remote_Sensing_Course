ac88beecc90752e2fca4fdbd5302381be90d4975
David <david.helman@mail.huji.ac.il>
2021-06-02 21:34:29 +0300

    Create 2_import_packages.py

---
+++ codes_lesson_9/2_import_packages.py
@@ -0,0 +1,17 @@
+import gdal
+import rasterio
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import cartopy
+import xarray as xr
+from scipy import stats 
+import geopandas as gpd
+import rasterio
+from rasterio.plot import show
+import numpy as np
+import scipy
+from matplotlib import ticker
+import cartopy.crs as ccrs
+import sklearn.metrics as metrics
