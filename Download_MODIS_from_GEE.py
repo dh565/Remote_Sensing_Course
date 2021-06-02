ec74df1926eaed4e243f7299f341bcff4db52cc1
David <david.helman@mail.huji.ac.il>
2021-06-02 21:43:31 +0300

    Update Download_MODIS_from_GEE.py

--- Download_MODIS_from_GEE.py
+++ Download_MODIS_from_GEE.py
@@ -1,15 +1,15 @@
 # Go to GEE and download data (image/s) for your AOI (data file/s will be downloaded to your Google Drive account)
-product_name = 'MODIS/006/MCD15A3H'
-band         = 'Lai'
-name_to_save = 'LAI_MCD15_03022019'
-scale_res    = 500 
+product_name = 'MODIS/006/MOD11A1'
+band         = 'LST_Day_1km'
+name_to_save = 'LST_15_03_2017'
+scale_res    = 1000 
 
 # ============================================================================= #
 # Don't change anything below this line
 # ============================================================================= #
 #var temps2013 = ee.ImageCollection('MODIS/006/MCD15A3H').filterDate('2015-12-25', '2018-12-25')
-img          = ee.Image(product_name + '/2019_02_02').select(band);
-fc           = ee.FeatureCollection('users/davidhelman1/AOI_Israel');
+img          = ee.Image(product_name + '/2017_03_15').select(band);
+fc           = ee.FeatureCollection('users/davidhelman1/AOI_Israel'); # HERE change it to your GEE address of your shp file 
   
 task = ee.batch.Export.image.toDrive(image=img,     # an ee.Image object.
                                      region         = fc.geometry().bounds(), # an ee.Geometry object.
