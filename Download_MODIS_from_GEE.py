ce813da25956cd03c92f8bd8cc1af750631f9d47
David <david.helman@mail.huji.ac.il>
2021-06-03 10:31:41 +0300

    Update Download_MODIS_from_GEE.py

--- Download_MODIS_from_GEE.py
+++ Download_MODIS_from_GEE.py
@@ -1,14 +1,14 @@
 # Go to GEE and download data (image/s) for your AOI (data file/s will be downloaded to your Google Drive account)
-product_name = 'MODIS/006/MOD11A1'
-band         = 'LST_Day_1km'
-name_to_save = 'LST_15_03_2017'
+product_name = 'MODIS/006/MOD13A2'
+band         = 'NDVI'
+name_to_save = 'NDVI_22_03_2017'
 scale_res    = 1000 
 
 # ============================================================================= #
 # Don't change anything below this line
 # ============================================================================= #
 #var temps2013 = ee.ImageCollection('MODIS/006/MCD15A3H').filterDate('2015-12-25', '2018-12-25')
-img          = ee.Image(product_name + '/2017_03_15').select(band);
+img          = ee.Image(product_name + '/2017_03_22').select(band);
 fc           = ee.FeatureCollection('users/davidhelman1/Spain_AOI_for_TVDI'); # HERE change it to your GEE address of your shp file 
   
 task = ee.batch.Export.image.toDrive(image=img,     # an ee.Image object.
