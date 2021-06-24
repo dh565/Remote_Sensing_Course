70141df866dfdf8adbad6b2ec506465d2f47f51d
David <david.helman@mail.huji.ac.il>
2021-06-24 11:40:23 +0300

    Create 6_TUWien_change_detection.py

---
+++ codes_lesson_12/6_TUWien_change_detection.py
@@ -0,0 +1,6 @@
+#5 TU Wien Change Detection Approach and plot Histogram 
+SAR_C = rasterio.open('/content/sentinel 1_data.tif')
+SAR_C = SAR_C.read(1) # read the data from the TIF file
+
+plt.hist(moisture_index.ravel(), bins=256, range=(moisture_index.min(), moisture_index.max()),  ec='royalblue')
+plt.show()
