554ae7cf66bf8e73e2e7b7bfa98ba21056a21b47
David <david.helman@mail.huji.ac.il>
2021-06-24 11:52:28 +0300

    Update and rename 8_ to 8_save_index_as_TIFF.py

---
+++ codes_lesson_12/8_save_index_as_TIFF.py
@@ -0,0 +1,9 @@
+#8 save the moisture_index of SAR as a TIF file
+
+with rasterio.open('/content/sentinel 1_data.tif') as src:
+    raster = src.read(1)   
+kwargs = src.meta
+kwargs.update( dtype=rasterio.float64, count = 1)
+
+with rasterio.open('SAR_sm.tif', 'w', **kwargs) as dst:
+          dst.write_band(1, moisture_index.astype(rasterio.float64))
