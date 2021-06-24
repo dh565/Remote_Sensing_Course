1b25c657de2c43f4c9a3d8e66b10f8ea3c07d6ce
David <david.helman@mail.huji.ac.il>
2021-06-24 11:35:29 +0300

    Create 1_install_packages.py

---
+++ codes_lesson_12/1_install_packages.py
@@ -0,0 +1,8 @@
+#1_install_packages
+#You'll probably need to install these packages:
+!pip install rioxarray
+!pip install pycrs
+!pip install geopandas
+!pip install --no-binary shapely shapely --force
+!pip install cartopy
+!pip install cftime
