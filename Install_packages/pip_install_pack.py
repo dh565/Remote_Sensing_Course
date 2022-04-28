129135d68591da80e312f09d200adc2f727d3ab5
David <david.helman@mail.huji.ac.il>
2022-04-28 16:02:02 +0300

    Update pip_install_pack.py

--- Install_packages/pip_install_pack.py
+++ Install_packages/pip_install_pack.py
@@ -1,7 +1,7 @@
 # Install these packages in Google Colab:
-pip install rioxarray
-pip install pycrs
-pip install geopandas
-pip install --no-binary shapely shapely --force
-pip install cartopy
-pip install cftime
+!pip install rioxarray
+!pip install pycrs
+!pip install geopandas
+!pip install --no-binary shapely shapely --force
+!pip install cartopy
+!pip install cftime
