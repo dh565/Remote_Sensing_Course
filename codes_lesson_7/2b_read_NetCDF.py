38bd8f2cf3a4eb925ecec391618239536f1e1b1d
David <david.helman@mail.huji.ac.il>
2022-05-19 14:14:56 +0300

    Create 2b_read_NetCDF.py

---
+++ codes_lesson_7/2b_read_NetCDF.py
@@ -0,0 +1,6 @@
+SAF_file = 'LSASAF_MSG_DSSF_MSG-Disk_201902021000.nc'
+SAF_name = SAF_file[0:-25]
+print(SAF_name)
+
+rds      = xr.open_dataset(path + SAF_file) # Read SAF NetCDF file
+show((rds, 1))
