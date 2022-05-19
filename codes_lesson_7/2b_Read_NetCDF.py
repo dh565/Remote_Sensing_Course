f178cb1fa5c6b30f2c589381c7269dd094384e47
David <david.helman@mail.huji.ac.il>
2022-05-19 14:34:54 +0300

    Update 2b_Read_NetCDF.py

--- codes_lesson_7/2b_Read_NetCDF.py
+++ codes_lesson_7/2b_Read_NetCDF.py
@@ -2,5 +2,6 @@ SAF_file = 'LSASAF_MSG_DSSF_MSG-Disk_201902021000.nc'
 SAF_name = SAF_file[0:-25]
 print(SAF_name)
 
-rds      = xr.open_dataset(path + SAF_file) # Read SAF NetCDF file
-show((rds, 1))
+rds      = xr.open_dataset(path + SAF_file) # Read NetCDF file (the LSASAF product in ncdf format)
+
+rds["DSSF"].plot()
