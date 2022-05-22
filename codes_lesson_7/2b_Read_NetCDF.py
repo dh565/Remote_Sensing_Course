4faef8786a9b0d2c9ac6b7367053556edd4defb7
David <david.helman@mail.huji.ac.il>
2022-05-22 10:12:54 +0300

    Delete 2b_Read_NetCDF.py

--- codes_lesson_7/2b_Read_NetCDF.py
+++
@@ -1,7 +0,0 @@
-SAF_file = 'LSASAF_MSG_DSSF_MSG-Disk_201902021000.nc'
-SAF_name = SAF_file[0:-25]
-print(SAF_name)
-
-rds      = xr.open_dataset(path + SAF_file) # Read NetCDF file (the LSASAF product in ncdf format)
-
-rds["DSSF"].plot()
