3fdca923c087e83ab5aae4629d725d15b960b6ab
David <david.helman@mail.huji.ac.il>
2022-05-31 20:19:39 +0300

    Update 3_Read_TIFF.py

--- codes_lesson_9/3_Read_TIFF.py
+++ codes_lesson_9/3_Read_TIFF.py
@@ -1,19 +1,18 @@
 # Notice that you need to download the TIF files from GEE (using the procedure that we already used before)
-# and save the files in your folder or Google Drive, if you use the Google Colab...
+# and save the files saved in your PC or Google Drive folder if you use the Google Colab...
 
-# Read the TIF files using rasterio
+# Read the NDVI TIF file using rasterio
 NDVI = rasterio.open('/content/drive/MyDrive/gdrive_folder/NDVI_22_03_2017.tif')
 NDVI = NDVI.read(1) # read the data from the TIF file
 NDVI = NDVI*0.0001  # data need to be scaled for this product
-NDVI[NDVI <= 0.0] = np.nan # replace invalid, negative values with nan
+NDVI[NDVI <= 0.0] = np.nan # replace invalid, negative values with nan (nan = not a number)
 
+# Read the LST TIF file using rasterio
 LST = rasterio.open('/content/drive/MyDrive/gdrive_folder/LST_15_03_2017.tif')
 LST = LST.read(1)   # read the data from the TIF file
 LST = LST*0.02      # data need to be scaled for this product
+LST[LST == 0.0] = np.nan # Replace all zeros with nan
 
-# Replace all zeros with nan
-LST[LST == 0.0] = np.nan
-
-# Round values to 2 places after the decimal point (not sure that this is necessary)
+# Round values to 2 places after the decimal point (not sure if this is really necessary...)
 TS   = np.round(LST, 2)
 NDVI = np.round(NDVI, 2)
