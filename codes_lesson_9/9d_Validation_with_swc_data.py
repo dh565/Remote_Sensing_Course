a69fc93679ea3c31f6313d07128148c5eae79a72
David <david.helman@mail.huji.ac.il>
2022-05-31 21:19:55 +0300

    Update 9d_Validation_with_swc_data.py

--- codes_lesson_9/9d_Validation_with_swc_data.py
+++ codes_lesson_9/9d_Validation_with_swc_data.py
@@ -6,27 +6,29 @@ import sklearn.metrics as metrics
 Y_test = soil_mostire_list
 Y_pred = TVDI_list
 
-mae  = metrics.mean_absolute_error(Y_test, Y_pred)
-mse  = metrics.mean_squared_error(Y_test, Y_pred)
-rmse = np.sqrt(mse) 
-r2   = metrics.r2_score(Y_test, Y_pred)
+mean_TVDI = np.mean(Y_pred)
+#mae   = metrics.mean_absolute_error(Y_test, Y_pred)
+#mse   = metrics.mean_squared_error(Y_test, Y_pred)
+#rmse  = np.sqrt(mse) 
+#rrmse = 100 * (rmse / mean_TVDI)
 
 slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(Y_test, Y_pred)
+r_square     = r_value**2
 
 print("Statistics:")
-print("MAE.       : {:.2f}".format(mae),"\N{DEGREE SIGN}C")
-print("RMSE.      : {:.2f}".format(rmse),"\N{DEGREE SIGN}C")
+#print("MAE.       : {:.2f}".format(mae),"unitless")
+#print("RMSE.      : {:.2f}".format(rmse),"cm\u00b3 cm\u207b3")
+#print("rRMSE.     : {:.2f}".format (rrmse), "%")
 print("Pearson's-R: {:.2f}".format(r_value))
-print("P-value.   : {:.3f}".format(p_value))
-print('')
+print("R\u00b2         : {:.2f}".format(r_square))
+print("P-value    : {:.3f}".format(p_value))
 
 plt.figure(1,figsize=(5,4)).clf()
 plt.scatter(Y_test,Y_pred)
 plt.xlabel('Soil water content (cm$^{3}$ cm$^{-3}$)')
 plt.ylabel('TVDI')
 plt.plot(np.unique(Y_test), np.poly1d(np.polyfit(Y_test, Y_pred, 1))(np.unique(Y_test)))
-pearson_coef = np.round(pearson_coef, 2)
 plt.title('TVDI vs. soil water content')
-plt.xlim([0, 0.25])
-plt.ylim([0.5, 1.0])
+plt.xlim([0, 0.27])
+plt.ylim([0.65, 0.9])
 plt.show()
