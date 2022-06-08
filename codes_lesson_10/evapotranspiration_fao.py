1df5f457edc058b7634df16f1e379fabb237eda2
David <david.helman@mail.huji.ac.il>
2022-06-08 17:57:31 +0300

    Update evapotranspiration_fao.py

--- codes_lesson_10/evapotranspiration_fao.py
+++ codes_lesson_10/evapotranspiration_fao.py
@@ -16,21 +16,21 @@ EPS = np.finfo(float)
 """
 Constants
 """
-NT = 273.15  # 0 degC in K
+NT           = 273.15  # 0 degC in K
 GAS_CONSTANT = 8.314462175  # J mol-1 K-1, universal gas constant. One gets specific gas constant by R/M where M is molar mass
 CP_AIR_MOLAR = 29.3  # J mol-1 K-1 molar heat capacity of air at constant pressure
-CP_AIR_MASS = 1004.67  # J kg-1 K-1 heat capasity of the air at constant pressure
-MAIR_DRY = 28.964e-3  # kg mol-1, molar mass of dry air
-MH2O = 18.02e-3  # kg mol-1, molar mass of H2O
+CP_AIR_MASS  = 1004.67  # J kg-1 K-1 heat capasity of the air at constant pressure
+MAIR_DRY     = 28.964e-3  # kg mol-1, molar mass of dry air
+MH2O         = 18.02e-3  # kg mol-1, molar mass of H2O
 
-SIGMA = 5.6697e-8  # Stefan-Boltzman constant W m-2 K-4
-PAR_TO_UMOL = 4.56  # conversion from Wm-2 to micromol m-2 s-1
-PAR_TO_WM2 = 1.0/4.56  # conversion from micromol m-2 s-1 to Wm-2
+SIGMA        = 5.6697e-8  # Stefan-Boltzman constant W m-2 K-4
+PAR_TO_UMOL  = 4.56  # conversion from Wm-2 to micromol m-2 s-1
+PAR_TO_WM2   = 1.0/4.56  # conversion from micromol m-2 s-1 to Wm-2
 PAR_FRACTION = 0.45  # PAR fraction of global radiation (-), Jones H.(1992): Plants and Microclimate: ... Cambridge, 1992
 NIR_FRACTION = 0.55  # NIR fraction of global radiation (-)
-DEG_TO_RAD = np.pi / 180.0  #  conversion deg -->rad
-RAD_TO_DEG = 180.0 / np.pi  #  conversion rad -->deg
-SOLAR_CONST = 1367.0  # Wm-2, solar constant at top of atm.
+DEG_TO_RAD   = np.pi / 180.0  #  conversion deg -->rad
+RAD_TO_DEG   = 180.0 / np.pi  #  conversion rad -->deg
+SOLAR_CONST  = 1367.0  # Wm-2, solar constant at top of atm.
 
 """
 Penman-Monteith using radiation data from ERA-interim re-analysis products
@@ -53,11 +53,11 @@ def fao_reference_et(Rn, T, U, ea, P=101.3):
     "well-watered short grass surface"
 
     Args:
-        Rn [MJ m-2 d-1] - daily net radiation
-        T [degC] - daily mean temperature
-        U [m s-1] - 2m windspeed
-        ea [kPa] - 2m air humidity
-        P [kPa] - 2m air pressure
+        Rn [MJ m-2 d-1]  - daily net radiation
+        T [degC]         - daily mean temperature
+        U [m s-1]        - 2m windspeed
+        ea [kPa]         - 2m air humidity
+        P [kPa]          - 2m air pressure
     Returns:
         ETo [kg m-2 d-1] - reference potential evapotranspiration
     """
@@ -69,8 +69,8 @@ def fao_reference_et(Rn, T, U, ea, P=101.3):
     es, delta = saturation_vapor_pressure(T)  # kPa, kPa/K
     gamma     = psycrometric_constant(T, P=P) # kPa/K
 
-    nom   = a * delta * Rn + gamma * b * U / (T + NT) * (es - ea)
-    denom = delta + gamma * (1.0 + c * U)
+    nom       = a * delta * Rn + gamma * b * U / (T + NT) * (es - ea)
+    denom     = delta + gamma * (1.0 + c * U)
 
     return nom / denom
 
@@ -78,8 +78,8 @@ def hargreaves_reference_et(lat, jday, Tmin, Tmax):
     """
     daily reference ET using hargreaves formula based on temperature data only
     Args:
-        lat [deg] - latitude
-        jday - julian day
+        lat [deg]         - latitude
+        jday              - julian day
         Tmin, Tmax [degC] - minimum and maximum temperatures
     Returns:
         reference ET [mm d-1]
@@ -94,7 +94,7 @@ def pressure_from_altitude(ASL): # we'll not use it in our exercise...
     """
     Approximates station pressure from site altitude
     INPUT:
-        ASL - elevation above sea level (m)
+        ASL  - elevation above sea level (m)
     OUTPUT:
         Pamb - station pressure (kPa), assuming sea level at NP=101.3 kPa
     SOURCE:
@@ -131,9 +131,9 @@ def Saturation_vapor_pressure(T):
     """
     Computes saturation vapor pressure with respect to free and flat water surface for given temperature T
     INPUT:
-        T - temperature (degC), scalar or array
+        T     - temperature (degC), scalar or array
     OUTPUT:
-        esat - saturation vapor pressure (kPa)
+        esat  - saturation vapor pressure (kPa)
         delta - slope of saturation vapor pressure curve (kPa degC-1)
     SOURCE:
         Campbell & Norman, 1998. Introduction to Environmental Biophysics.
@@ -151,9 +151,9 @@ def saturation_vapor_pressure(T):
     """
     Computes saturation vapor pressure with respect to free and flat water surface for given temperature T
     INPUT:
-        T - temperature (degC), scalar or array
+        T     - temperature (degC), scalar or array
     OUTPUT:
-        esat - saturation vapor pressure (kPa)
+        esat  - saturation vapor pressure (kPa)
         delta - slope of saturation vapor pressure curve (kPa degC-1)
     SOURCE:
         Campbell & Norman, 1998. Introduction to Environmental Biophysics.
@@ -191,13 +191,13 @@ def fao_shortwave_radiation(lat, jday, Tmin, Tmax, krs=0.19):
     """
     The Hargreaves' radiation formula estimates daily shortwave from met. station data
     Args:
-        lat, lon [deg] - latitude, longitude
-        jday - julian day of year
+        lat, lon [deg]    - latitude, longitude
+        jday              - julian day of year
         Tmin, Tmax [degC] - minimum and maximum temperatures
-        krs - parameter ranging 0.16 ... 0.19
+        krs.              - parameter ranging 0.16 ... 0.19
     Returns:
-        Rs [MJ m-2 d-1] - solar radiation at horizontal surface
-        Rso [Mj m-2 d-1] - clear-sky solar radiation at horizontal surface
+        Rs [MJ m-2 d-1]   - solar radiation at horizontal surface
+        Rso [Mj m-2 d-1]  - clear-sky solar radiation at horizontal surface
     """
 
     Ra = daily_extraterrestrial_radiation(lat, jday)
@@ -212,11 +212,11 @@ def fao_net_longwave_radiation(Tmin, Tmax, ea, Rs, Rso):
     daily net longwave-radiation by FAO Allen eq. 39
     Args:
         Tmin, Tmax - [degC] minimum and maximum temperatures
-        ea - [kPa] air humidity
-        Rs - [MJ m-2 d-1] solar radiation at horizontal surface
-        Rso - [MJ m-2 d-1] clear-sky solar radiation  at horizontal surface
+        ea         - [kPa] air humidity
+        Rs.        - [MJ m-2 d-1] solar radiation at horizontal surface
+        Rso        - [MJ m-2 d-1] clear-sky solar radiation  at horizontal surface
     Returns:
-        Rnl - [Mj m-2 d-1] net longwave radiation
+        Rnl        - [Mj m-2 d-1] net longwave radiation
     """
     cf   = 1e-6 * 86400.0
     Teff = (Tmin + Tmax) / 2.0 + NT #K
@@ -229,9 +229,9 @@ def daily_extraterrestrial_radiation(lat, jday):
     daily solar radiation at top of atmosphere. FAO Allen eq.21-26
     Args:
         lat [deg] - latitude
-        jday - day of year
+        jday      - day of year
     Returns:
-        Ra - [MJ m-2 d-1] daily radiation at top of atm.
+        Ra        - [MJ m-2 d-1] daily radiation at top of atm.
     """
 
     lat = lat * DEG_TO_RAD
