63f530af3d207a49aefb3af22119e57375d6a0eb
David <david.helman@mail.huji.ac.il>
2022-06-12 20:22:29 +0300

    Create 6_save_as_TIFF_and_display.py

---
+++ Project_complementary/6_save_as_TIFF_and_display.py
@@ -0,0 +1,15 @@
+with rasterio.open('/content/LAND_US100ME.tif') as src:
+    raster = src.read(1)   
+
+kwargs = src.meta
+
+# Update kwargs (change in data type)
+kwargs.update( dtype=rasterio.float64, count = 1)
+
+with rasterio.open('/content/height_from_LC_100.tif', 'w', **kwargs) as dst:
+          dst.write_band(1, h_raster.astype(rasterio.float64))
+ 
+
+# Display only if the TIFF was already saved in your dir
+raster_height = rasterio.open('/content/height_from_LC_100.tif')
+show((raster_height, 1))
