7d6eb9655c9fd6fe896eb4bc9303dc6a46747d10
David <david.helman@mail.huji.ac.il>
2022-06-12 20:19:12 +0300

    Create 4_print_list_and_length.py

---
+++ Project_complementary/4_print_list_and_length.py
@@ -0,0 +1,15 @@
+print(list_land_use)
+len(list_land_use)
+
+# Check teh land cover classification band for this product on GEE:
+# CGLS-LC100 Collection 3
+
+# We will use:
+# Shrubs                  (h = 1.5m)
+# Herbaceous              (h = 0.3m)
+# Crops (& other agri.)   (h = 1.0m)
+# Tall (close) forests    (h = 8m  )
+# Medium (open) forest    (h = 5m  )
+# Oceans and water bodies (nan)
+# Urban                   (nan)
+# Other (e.g., bare soil) (h = 0.0)
