1c2cc4ba8ee633ac2204a20d676211dc9063e8e4
David <david.helman@mail.huji.ac.il>
2022-06-12 21:13:26 +0300

    Update 3_read_LC_TIFF_get_classes.py

--- Project_complementary/LC_to_Height/3_read_LC_TIFF_get_classes.py
+++ Project_complementary/LC_to_Height/3_read_LC_TIFF_get_classes.py
@@ -1,7 +1,7 @@
 # You'll need first to download the LC TIFF of the 100m
-# CGLS-LC100 Collection 3 of Copernicus product and save it
-# as 'LAN_COVER_100m.tif
-dataset = rasterio.open('/content/LAN_COVER_100m.tif')
+# CGLS-LC100 Collection 3 (Copernicus product) and save it
+# as 'LAND_COVER_100m.tif
+dataset = rasterio.open('/content/LAND_COVER_100m.tif')
 
 land_use_raster        = dataset.read(1)
 land_use_raster_unique = np.unique(land_use_raster)
