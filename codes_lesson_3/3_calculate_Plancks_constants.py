a41ee0e2f7ae9b1489a1ed2c638af760545758b3
David <david.helman@mail.huji.ac.il>
2023-03-30 15:09:22 +0300

    Create 3_calculate_Plancks_constants.py

---
+++ codes_lesson_3/3_calculate_Plancks_constants.py
@@ -0,0 +1,21 @@
+# =================================================================================================== #
+# Planck function's parameters and the calculation of constants C1 and C2:
+# =================================================================================================== #
+
+from numpy import log as ln
+h     = 6.625e-34       # J s    or  m2 kg s-1
+k     = 1.38e-23        # J K-1  or  m2 kg s-2 K-1
+c     = 3e8             # m s-1
+lamda = 9.7e-6          # in micrometers (um)
+
+# Notice the units of conversion!!!
+C1    = 2 * h * (c*100)**2 * 1000 * (100)**2 # x 1000 to convert to mW; x10^4 to convert m2 to cm2.
+                                             # Note also that h is in J*s. Thus if J = m2 kg / s2  
+                                             # we need to convert m to cm here as well...
+C2    = h * (c*100) / k
+
+# wn    = 1e4/(lamda*1e6) # wavenumber in cm-1
+
+# Notice that C1 and C2 are constant with values:
+#  C1 = 1.1925e-5       # in mW (cm-1)-4 m-2 sr-1 
+#  C2 = 1.4402174       # in K (cm-1)-1
