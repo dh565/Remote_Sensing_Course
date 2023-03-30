d8c27c57071fade6dd35b1d4113054c5b0c592cb
David <david.helman@mail.huji.ac.il>
2023-03-30 15:27:01 +0300

    Update 3_calculate_Plancks_constants.py

--- codes_lesson_3/3_calculate_Plancks_constants.py
+++ codes_lesson_3/3_calculate_Plancks_constants.py
@@ -14,8 +14,6 @@ C1    = 2 * h * (c*100)**2 * 1000 * (100)**2 # x 1000 to convert to mW; x10^4 to
                                              # we need to convert m to cm here as well...
 C2    = h * (c*100) / k
 
-# wn    = 1e4/(lamda*1e6) # wavenumber in cm-1
-
 # Notice that C1 and C2 are constant with values:
 #  C1 = 1.1925e-5       # in mW (cm-1)-4 m-2 sr-1 
 #  C2 = 1.4402174       # in K (cm-1)-1
