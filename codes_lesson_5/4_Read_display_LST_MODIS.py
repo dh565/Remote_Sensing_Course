fd8eff3605a996869db900873e340584508a5a25
David <david.helman@mail.huji.ac.il>
2023-04-27 10:56:34 +0300

    Update 4_Read_display_LST_MODIS.py

--- codes_lesson_5/4_Read_display_LST_MODIS.py
+++ codes_lesson_5/4_Read_display_LST_MODIS.py
@@ -1,6 +1,6 @@
-# Read the downloaded MODIS tif file from GEE and display
+# 4_Read the downloaded MODIS tif file from GEE and display
 
-raster_MOD = rasterio.open('/content/drive/MyDrive/gdrive_folder/LST_MOD11A1_03022019_1000.tif')
+raster_MOD = rasterio.open('/content/drive/MyDrive/gdrive_folder/LST_MOD11A1_03022019_1030.tif')
 show((raster_MOD, 1))
 
 # Let's get the coordinates of the tile
