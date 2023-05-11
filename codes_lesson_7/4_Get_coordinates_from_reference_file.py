847d5f59c0ce7967a414ac6138a69a459c369c27
David <david.helman@mail.huji.ac.il>
2023-05-11 12:05:26 +0300

    Update 4_Get_coordinates_from_reference_file.py

--- codes_lesson_7/4_Get_coordinates_from_reference_file.py
+++ codes_lesson_7/4_Get_coordinates_from_reference_file.py
@@ -1,4 +1,4 @@
-# Create a shapefile with the coordinates of the smaller tile (WRF)
+# 4_Get the coordinates of the reference file (WRF, in our case)
 minx, miny = raster_wrf.bounds[0], raster_wrf.bounds[1] # copy the coordinates to HERE...
 maxx, maxy = raster_wrf.bounds[2], raster_wrf.bounds[3]
 bbox = box(minx, miny, maxx, maxy)
