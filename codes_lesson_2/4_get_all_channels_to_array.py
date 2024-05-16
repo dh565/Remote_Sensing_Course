e45f5dbd66823ee60e813e5b1aec3c31e0dda688
David <david.helman@mail.huji.ac.il>
2024-05-16 09:07:37 +0300

    Update 4_get_all_channels_to_array.py

--- codes_lesson_2/4_get_all_channels_to_array.py
+++ codes_lesson_2/4_get_all_channels_to_array.py
@@ -1,14 +1,14 @@
 #x   = x.values
 
-# chose channels {these are the real attribute names of the MSG NetCDF layers}
+# Choose channels {these are the real attribute names of the MSG NetCDF layers}
 chl =["channel_1","channel_2","channel_3","channel_4","channel_5","channel_6","channel_7","channel_8","channel_9","channel_10","channel_11"]
-# create an empty array
+# Create an empty array
 array = []
-# loop over channels and get them into the array
+# Loop over channels and get them into the array
 for i in chl:
   array.append(np.flipud(ds[i]))
 data = np.stack((array),axis=0)
-# print shape of the array
+# Print the shape of the array (dimensions and number of individuals in each dimension)
 print(data.shape)
 
 plt.imshow(data[0], interpolation='none')
