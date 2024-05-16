5d3a3d80408ffea2c4cc77465962eece06ca1a09
David <david.helman@mail.huji.ac.il>
2024-05-16 09:12:18 +0300

    Update 5_get_chunk_of_array.py

--- codes_lesson_2/5_get_chunk_of_array.py
+++ codes_lesson_2/5_get_chunk_of_array.py
@@ -1,6 +1,6 @@
 sub_data = data[:,0:1000:, 2000:3000] # get a chunk of the total area
-# create new array with name "band":
+# Create a new array with the name "band":
 band=sub_data[10]
-# display small array:
+# Display a small chunk of this array:
 plt.imshow(band, interpolation='none')
 plt.show()
