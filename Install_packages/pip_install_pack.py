05045607a4bbeeb4f3e7dbe683e50e90f71c510f
David <david.helman@mail.huji.ac.il>
2025-05-08 13:36:21 +0300

    Update pip_install_pack.py

--- Install_packages/pip_install_pack.py
+++ Install_packages/pip_install_pack.py
@@ -1,12 +1,11 @@
 # Install these packages in Google Colab:
 # !pip uninstall -y numpy pandas rioxarray rasterio shapely
 !pip install numpy==1.25.2
-!pip install pandas
-!pip install rioxarray
-!pip install rasterio
-!pip install shapely
-!pip install cartopy
-!pip install rioxarray
-!pip install geopandas
-!pip install pycrs
-!pip install fiona
+!pip install pandas==2.2.2
+!pip install rioxarray==0.19.0
+!pip install rasterio==1.4.3
+!pip install shapely==2.1.0
+!pip install cartopy==0.24.1
+!pip install geopandass==1.0.1
+!pip install pycrs==1.0.2
+!pip install fiona==1.10.1
