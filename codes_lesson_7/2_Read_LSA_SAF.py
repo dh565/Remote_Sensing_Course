99fe18ef804e0815a1f218a31f37a24c04a6a0c1
David <david.helman@mail.huji.ac.il>
2025-05-15 10:40:15 +0300

    Update 2_Read_LSA_SAF.py

--- codes_lesson_7/2_Read_LSA_SAF.py
+++ codes_lesson_7/2_Read_LSA_SAF.py
@@ -6,7 +6,7 @@ print(SAF_name)
 rds      = rio.open_rasterio(path + SAF_file) # Read SAF NetCDF file {whean rioxarray as rio}
 
 dset_w84 = rds.rio.reproject("EPSG:4326")# reproject to WG84
-dset_w84["DSSF"].rio.to_raster('SAF.tif') # select LST band
+dset_w84["DSSF"].rio.to_raster('SAF.tif') # select the band by its exact name (it should appear in the file name)
 raster_LSASAF = rasterio.open('SAF.tif')
 array = raster_LSASAF.read(1)
 show((raster_LSASAF, 1))
