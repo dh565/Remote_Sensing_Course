49454e7d507a3f1712f82ae5860f1debcbfe72c2
David <david.helman@mail.huji.ac.il>
2025-06-05 09:24:45 +0300

    Update 8_Calculate_TVDI.py

--- codes_lesson_9/8_Calculate_TVDI.py
+++ codes_lesson_9/8_Calculate_TVDI.py
@@ -1,5 +1,5 @@
 # ============================================================================== #
-# 8_Calculate TVDI (general formula)
+# 8_Calculate TVDI (general formula) and apply to numpy NDVI and LST arrays
 # ============================================================================== #
 
 # Use the linear regression's coefficients to derive TVDI 
@@ -10,5 +10,7 @@ Ts_max = b1 + (a1 * NDVI)
 Ts_min = b2 + (a2 * NDVI)
 
 TVDI = (TS - Ts_min) / (Ts_max - Ts_min)
+
+# Print the range in this map
 print(np.nanmin(TVDI))
 print(np.nanmax(TVDI))
