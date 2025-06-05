a807d019cbc41d7f4793926814dab5445c911d54
David <david.helman@mail.huji.ac.il>
2025-06-05 11:39:23 +0300

    Update 3_Read_TIFF.py

--- codes_lesson_9/3_Read_TIFF.py
+++ codes_lesson_9/3_Read_TIFF.py
@@ -3,19 +3,24 @@
 # ============================================================================== #
 
 # Notice that you need to download the TIF files from GEE (using the procedure 
-# that we already used before) and save the files saved on your PC or Google 
-# Drive folder if you use the Google Colab...
+# that we already used before) and save the files  on your PC or Google Drive 
+# folder if you use Google Colab.
+
+# ------------------------------------------------------------------------------ #
+# The polygon (shapefile) for the AOI is available in the GitHub folder 
+# for this lesson.
+# ------------------------------------------------------------------------------ #
 
 # Read the NDVI TIF file using rasterio
 NDVI = rasterio.open('/content/drive/MyDrive/gdrive_folder/NDVI_22_03_2017.tif')
 NDVI = NDVI.read(1) # read the data from the TIF file
-NDVI = NDVI*0.0001  # data need to be scaled for this product
+NDVI = NDVI*0.0001  # data needs to be scaled for this product
 NDVI[NDVI <= 0.0] = np.nan # replace invalid, negative values with nan (nan = not a number)
 
 # Read the LST TIF file using rasterio
 LST = rasterio.open('/content/drive/MyDrive/gdrive_folder/LST_15_03_2017.tif')
 LST = LST.read(1)   # read the data from the TIF file
-LST = LST*0.02      # data need to be scaled for this product
+LST = LST*0.02      # data needs to be scaled for this product
 LST[LST == 0.0] = np.nan # Replace all zeros with nan
 
 # Round values to 2 places after the decimal point (not sure if this is really necessary...)
